f5ba8720ca606b7f54ea9b34adc7a851f9152cce
lyhoangthi <102993502+lyhoangthi@users.noreply.github.com>
2022-04-06 15:26:07 +0200

    yaaaa

--- code/task-1/sketch.js
+++ code/task-1/sketch.js
@@ -7,5 +7,73 @@ function setup() {
 }
 
 function draw() {
-  background(234, 0,50);
+  background(245,227,29);
+
+  fill(9,0,250);
+  arc(200,200,
+    600,600,
+    Math.PI/180*-90,
+    Math.PI/180*0);
+
+  fill(76,26,250);
+  arc(200,200,
+    700,700,
+    Math.PI/180*-60,
+    Math.PI/180*0);
+
+  strokeWeight(0);
+  fill(129,21,250);
+  arc(200,200,
+    600,600,
+    Math.PI/180*-30,
+    Math.PI/180*0);
+
+  
+  strokeWeight(0);
+  fill(129,21,250);
+  arc(200,200,
+    600,600,
+    Math.PI/180*30,
+    Math.PI/180*-180);
+
+
+  strokeWeight(0);
+  fill(56, 126, 255);
+  circle (200,200,285);
+  
+  strokeWeight(0);
+  fill(174, 222, 91);
+  circle (200,200,200);
+  
+  strokeWeight(0);
+  fill(250, 123, 54);
+  rect (200, 200, 300, 100);
+  rect (200, 200, 100, 300);
+  rect (100, 0, 100, 200);
+  rect (0, 100, 200, 100);
+
+  fill(175,56,255);
+  circle(350,350,100);
+
+  fill(175,56,255);
+  circle(50,50,100);
+
+  strokeWeight (50);
+  stroke(250,255,250);
+  point(50,50);
+
+  strokeWeight (7);
+  stroke(250,255,250);
+  point(350,350);
+  
+  fill('rgba(0,0,0,0)');
+  strokeWeight (50);
+  stroke(250,255,250);
+  arc(0,400,
+    700,650,
+    Math.PI/180*270,
+    Math.PI/180*200);
+
+
+
 }
